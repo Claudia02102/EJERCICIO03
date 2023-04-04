f06027036bc5ccf92c56ac41fb93ccace7b4df24
cpa02 <cpa02@DESKTOP-001F323>
2023-04-04 05:19:01 -0500

    Agregar archivos de proyecto.

---
+++ EJERCICIO03/Controllers/E3Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EJERCICIO03.Controllers
+{
+    public class E3Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EJERCICIO03/wwwroot/js/procesar.js
@@ -0,0 +1,17 @@
+﻿
+var cant = document.getElementById("cantidad");
+var prec = document.getElementById("precio");
+
+function getOperacion() {
+
+    var Importe_Compra = parseFloat(prec.value) * parseInt(cant.value);
+    var primer_dsct = 0.10 * Importe_Compra;
+    var segundo_dsct = 0.10 * (Importe_Compra - primer_dsct);
+    var Importe_DsctTotal = (primer_dsct + segundo_dsct).toFixed(2);
+    var Importe_Pagar = Importe_Compra - Importe_DsctTotal;
+
+    document.getElementById("resultados").value = "Importe de la Compra: " + Importe_Compra + "\n" +
+                                                  "Importe del Descuento total: " + Importe_DsctTotal + "\n" +
+                                                  "Importe a Pagar: " + Importe_Pagar;
+    return false;
+}
